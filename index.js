55ee45031c832defeaa5d72f372310f2ecdb89ec
Saravanan E <120277637+Saravanan-E-dev@users.noreply.github.com>
2025-07-26 18:30:37 +0000

    intital commit - D1

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+import dotenv from 'dotenv';
+dotenv.config();
 
 import express from 'express';
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+// console.log(PORT, MONGO_URI);
+
+
+//connect to mongodb
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.json({ message: 'working' });
 });
 //start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 

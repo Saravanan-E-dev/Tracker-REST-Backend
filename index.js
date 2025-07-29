509852bda09ac3c45ad7ea8e1b5db2fcf53e31b3
Saravanan E <120277637+Saravanan-E-dev@users.noreply.github.com>
2025-07-29 19:27:14 +0000

    backend stuffs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import e from 'express';
 dotenv.config();
 
 import express from 'express';
@@ -22,10 +23,49 @@ mongoose.connect(MONGO_URI, {
 });
 
 const app = express();
+app.use(express.json());
+
+const ExpenseDataSchema = mongoose.Schema({
+  amount: Number,
+  remark: String,
+  spentFromState: String,
+})
+
+const ExpenseData = mongoose.model('ExpenseData', ExpenseDataSchema);
 
 app.get('/', (req, res) => {
   res.json({ message: 'working' });
 });
+
+//function saveExpenseData(objData)
+app.post('/api/save/ExpenseData', async(req, res) => {
+  try{
+    const {amount,remark,spentFromState} = req.body; 
+    const newExpenseData = new ExpenseData({
+      amount,
+      remark,
+      spentFromState
+    });
+    const savedExpenseData = await newExpenseData.save();
+    res.status(200).json({ message: 'Expense data saved successfully' },savedExpenseData);
+  }catch (error) {
+    console.error('Error saving expense data:', error);
+    res.status(500).json({ message: 'Error saving expense data' },error);
+  }
+  
+});
+
+//export function getExpenseData(DataName)
+app.get('/api/get/ExpenseData', async(req, res) => {
+  try {
+    const expenseData = await ExpenseData.find();
+    res.status(200).json(expenseData);
+  } catch (error) {
+    console.error('Error fetching expense data:', error);
+    res.status(500).json({ message: 'Error fetching expense data' },error);
+  }
+});
+
 //start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);

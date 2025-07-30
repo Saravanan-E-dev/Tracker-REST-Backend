2e8495c0025b36ebd20d15d3357894815b273c95
Saravanan E <120277637+Saravanan-E-dev@users.noreply.github.com>
2025-07-30 18:45:33 +0000

    some endpoints

--- index.js
+++ index.js
@@ -30,7 +30,14 @@ const ExpenseDataSchema = mongoose.Schema({
   remark: String,
   spentFromState: String,
 })
+const TransactionsSchema = mongoose.Schema({
+  amount: Number,
+  remark: String,
+  medium: String,
+  // date: { type: Date, default: Date.now }
+});
 
+const TransactionsData = mongoose.model('TransactionsData', TransactionsSchema);
 const ExpenseData = mongoose.model('ExpenseData', ExpenseDataSchema);
 
 app.get('/', (req, res) => {
@@ -65,6 +72,36 @@ app.get('/api/get/ExpenseData', async(req, res) => {
     res.status(500).json({ message: 'Error fetching expense data' },error);
   }
 });
+
+//export function setTransactions(DataObject)
+app.post('/api/save/setTransactions', async(req, res) => {
+  try{
+    const {amount, remark, medium} = req.body;
+    const newTransaction = new TransactionsData({
+      amount,
+      remark,
+      medium
+    });
+    const savedTransaction = await newTransaction.save();
+    res.status(200).json({ message: 'Transaction saved successfully' }, savedTransaction);
+  }catch (error) {
+    console.error('Error saving transactions:', error);
+    res.status(500).json({ message: 'Error saving transactions' },error);
+  }
+});
+
+//export function getTransactions()
+app.get('/api/get/TransactionsData', async(req, res) => {
+  try {
+    const transactionsData = await TransactionsData.find().sort({createdAt: -1});
+    res.status(200).json(transactionsData);
+  } catch (error) {
+    console.error('Error fetching transactions data:', error);
+    res.status(500).json({ message: 'Error fetching transactions data' },error);
+  }
+});
+
+//Balance API
 
 //start the server
 app.listen(PORT, () => {

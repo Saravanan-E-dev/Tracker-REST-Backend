621fe7c8eee893c3fb86bc356775846ee66ece33
Saravanan E <120277637+Saravanan-E-dev@users.noreply.github.com>
2025-07-31 20:02:41 +0000

    balance backend

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import e from 'express';
 dotenv.config();
 
 import express from 'express';
@@ -26,17 +25,28 @@ const app = express();
 app.use(express.json());
 
 const ExpenseDataSchema = mongoose.Schema({
+  userId: String,
   amount: Number,
   remark: String,
   spentFromState: String,
+  date: { type: Date, default: Date.now }
 })
 const TransactionsSchema = mongoose.Schema({
+  userId: String,
   amount: Number,
   remark: String,
   medium: String,
-  // date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now }
 });
 
+const BalanceSchema = mongoose.Schema({
+  userId: String,
+  OnlineBalance: Number,
+  OfflineBalance: Number,
+  TotalBalance: Number,
+});
+
+const BalanceData = mongoose.model('BalanceData', BalanceSchema);
 const TransactionsData = mongoose.model('TransactionsData', TransactionsSchema);
 const ExpenseData = mongoose.model('ExpenseData', ExpenseDataSchema);
 
@@ -93,7 +103,7 @@ app.post('/api/save/setTransactions', async(req, res) => {
 //export function getTransactions()
 app.get('/api/get/TransactionsData', async(req, res) => {
   try {
-    const transactionsData = await TransactionsData.find().sort({createdAt: -1});
+    const transactionsData = await TransactionsData.find().sort({ createdAt: -1 });
     res.status(200).json(transactionsData);
   } catch (error) {
     console.error('Error fetching transactions data:', error);
@@ -102,6 +112,56 @@ app.get('/api/get/TransactionsData', async(req, res) => {
 });
 
 //Balance API
+//AddOnlineBalance(BalanceData),
+//AddOfflineBalance(BalanceData)
+app.post('/api/save/balance/:userId', async(req, res) => {
+  try {
+    const { userId } = req.params;
+    const { OnlineBalance,OfflineBalance,TotalBalance } = req.body;
+    const UpdatedTotalBalance = OnlineBalance + OfflineBalance;
+
+    // update or create balance data
+    const existingBalanceData = await BalanceData.findOne({ userId });
+    if (existingBalanceData) {
+      existingBalanceData.OnlineBalance = OnlineBalance;
+      existingBalanceData.OfflineBalance = OfflineBalance;
+      existingBalanceData.TotalBalance = UpdatedTotalBalance;
+      await existingBalanceData.save();
+      return res.status(200).json({ message: 'Balance data updated successfully' }, existingBalanceData);
+    }
+
+    const newBalanceData = new BalanceData({
+      userId,
+      OnlineBalance,
+      OfflineBalance,
+      UpdatedTotalBalance
+    });
+
+    const savedBalanceData = await newBalanceData.save();
+    res.status(200).json({ message: 'Balance data saved successfully' },savedBalanceData);
+  } catch (error) {
+    console.error('Error saving balance:', error);
+    res.status(500).json({ message: 'Error fetching balance' }, error);
+  }
+});
+
+
+//getTotalBalance()
+//getOnlineBalance()
+//getOfflineBalance()
+app.get('/api/get/balance/:userId', async(req, res) => {
+  try {
+    const { userId } = req.params;
+    const balanceData = await BalanceData.findOne({ userId });
+    if (!balanceData) {
+      return res.status(404).json({ message: 'Balance data not found' });
+    }
+    res.status(200).json(balanceData);
+  } catch (error) {
+    console.error('Error fetching balance data:', error);
+    res.status(500).json({ message: 'Error fetching balance data' }, error);
+  }
+});
 
 //start the server
 app.listen(PORT, () => {
